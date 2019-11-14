830b02a028eb113c0376a5caa021108216b392cf
yantene <contact@yantene.net>
2019-11-14 13:48:33 +0900

    自分以外が作成したリストに関しても見られるように

--- app/bot/feature/list.rb
+++ app/bot/feature/list.rb
@@ -42,7 +42,7 @@ module Bot
         end
 
         # リストが存在していなくても行える操作はここまで
-        unless list = user.lists.find_by(name: list_name)
+        unless list = List.find_by(name: list_name)
           post(I18n.t('features.list.list_not_found.', list_name: list_name, locale: user.locale), data)
           return true
         end
